808c7d8d0680897f13dbf9ccb927c86ea2dd8033
yoonminsang <apdlyooapdl@naver.com>
2021-11-24 22:26:33 +0900

    feat: 메뉴 컴포넌트 생성

---
+++ src/components/menu/index.ts
@@ -0,0 +1,25 @@
+import Component from '@/lib/component';
+import { useHistory } from '@/lib/routerHooks';
+import './style.css';
+
+class Menu extends Component {
+  markup() {
+    const history = useHistory();
+    const menuList = [
+      { name: 'API', pathname: '/' },
+      { name: '로컬', pathname: '/local' },
+    ];
+    return /* html */ `
+      <div class="flex full-width menu">
+        ${menuList
+          .map(({ name, pathname }) => {
+            const selected = pathname === history.pathname;
+            return `<a href=${pathname} ${selected ? 'class="selected"' : ''}>${name}</a>`;
+          })
+          .join('')}
+      </div>
+    `;
+  }
+}
+
+export default Menu;
