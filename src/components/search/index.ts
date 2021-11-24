0439be999bf5fb3b09cf6cae0daf167ec5ebcb55
yoonminsang <apdlyooapdl@naver.com>
2021-11-25 03:31:30 +0900

    feat: 검색 컴포넌트 생성

---
+++ src/components/search/index.ts
@@ -0,0 +1,51 @@
+import Component from '@/lib/component';
+import githubStore from '@/store/github-store';
+import Input from '../common/input';
+import './style.css';
+
+class Search extends Component {
+  setup() {
+    this.state = {
+      search: '',
+    };
+  }
+
+  markup() {
+    let iconClass = 'icon search';
+    iconClass += `${this.state.search ? '-black' : '-grey'}`;
+    return /* html */ `
+      <form class="search-form">
+        <div class="flex">
+          <inside class="input-inside"></inside>
+          <button class="${iconClass}" type="submit"></button>
+        </div>
+      </form>
+    `;
+  }
+
+  appendComponent(target: HTMLElement | DocumentFragment) {
+    const $input = target.querySelector('.input-inside') as HTMLElement;
+    new Input($input, {
+      placeholder: '검색어를 입력하세요',
+      required: true,
+      value: this.state.search,
+      class: 'input-search full-width',
+      type: 'text',
+    });
+  }
+
+  setEvent() {
+    this.addEvent('submit', '.search-form', async (e: Event) => {
+      e.preventDefault();
+      const { search } = this.state;
+      const encodeSearch = encodeURIComponent(search);
+      githubStore.getGithubUsers(encodeSearch);
+    });
+    this.addEvent('input', '.input-search', (e: Event) => {
+      const target = e.target as HTMLInputElement;
+      this.setState({ search: target.value });
+    });
+  }
+}
+
+export default Search;
