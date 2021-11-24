f5199a54e35e590a5df8d98e75809d5f95696000
yoonminsang <apdlyooapdl@naver.com>
2021-11-25 03:32:57 +0900

    feat: github 서치 페이지 생성

---
+++ src/pages/github-search-page.ts
@@ -0,0 +1,23 @@
+import Header from '@/components/common/header';
+import SearchTemplate from '@/components/search-template';
+import Component from '@/lib/component';
+
+class GithubSearchPage extends Component {
+  markup() {
+    return /* html */ `
+    <div class="wrapper">
+      <inside class="header-inside"></inside>
+      <inside class="search-template-inside"></inside>
+    </div>
+    `;
+  }
+
+  appendComponent(target: HTMLElement | DocumentFragment) {
+    const $header = target.querySelector('.header-inside') as HTMLElement;
+    const $githubSearch = target.querySelector('.search-template-inside') as HTMLElement;
+    new Header($header);
+    new SearchTemplate($githubSearch);
+  }
+}
+
+export default GithubSearchPage;
