a2906961dea3952a6a2d77fb476b3e9f86c6bd86
yoonminsang <apdlyooapdl@naver.com>
2021-11-24 19:09:15 +0900

    feat: 웹팩 바벨 설정

---
+++ config/webpack.dev.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 9000,
+    hot: true,
+    historyApiFallback: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+      },
+    },
+  },
+});
