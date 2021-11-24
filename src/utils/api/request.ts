58b624c41d26c38e8d9df13a77bbc738e6af9fa9
yoonminsang <apdlyooapdl@naver.com>
2021-11-24 22:28:31 +0900

    feat: axios 모듈함수 생성

---
+++ src/utils/api/request.ts
@@ -0,0 +1,34 @@
+import axios, { AxiosInstance, AxiosResponse } from 'axios';
+
+type Method = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
+
+const client: AxiosInstance = axios.create();
+
+client.defaults.baseURL = 'https://api.github.com/';
+client.defaults.withCredentials = true;
+
+const requestEvent = new CustomEvent('request');
+const requestEndEvent = new CustomEvent('request-end');
+
+async function request<T>(method: Method, url: string, body?: T, multipart?: boolean): Promise<AxiosResponse> {
+  window.dispatchEvent(requestEvent);
+  const multipartOption = multipart && { 'Content-Type': 'multipart/form-data' };
+  try {
+    const res = await client({
+      method,
+      url,
+      headers: {
+        ...multipartOption,
+      },
+      ...(body && { data: body }),
+    });
+    return res;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  } finally {
+    window.dispatchEvent(requestEndEvent);
+  }
+}
+
+export default request;
