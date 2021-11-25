cb7338953bdf8b0e97a36b81bf7a6221067c0095
yoonminsang <apdlyooapdl@naver.com>
2021-11-26 02:36:58 +0900

    feat: 404 컴포넌트 구현

--- src/pages/not-found-page.ts
+++ src/pages/not-found-page.ts
@@ -1,10 +1,22 @@
+import Header from '@/components/common/header';
+import NotFound from '@/components/not-found';
 import Component from '@/lib/component';
 
 class NotFoundPage extends Component {
   markup() {
     return /* html */ `
-      <div>404 페이지~~</div>
+    <div class="wrapper">
+      <inside class="header-inside"></inside>
+      <inside class="not-found-inside"></inside>
+    </div>
     `;
+  }
+
+  appendComponent(target: HTMLElement | DocumentFragment) {
+    const $header = target.querySelector('.header-inside') as HTMLElement;
+    const $notFound = target.querySelector('.not-found-inside') as HTMLElement;
+    new Header($header);
+    new NotFound($notFound);
   }
 }
 
